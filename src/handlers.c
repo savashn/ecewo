a76e77236a46b37f1e94b44d5da695cdcfc280be
savashn <savashn@proton.me>
2025-05-31 03:49:25 +0300

    update

--- src/handlers.c
+++
@@ -1,6 +0,0 @@
-#include "handlers.h"
-
-void hello_world(Req *req, Res *res)
-{
-  text(200, "hello world!");
-}
--- src/handlers.h
+++
@@ -1,6 +0,0 @@
-#ifndef HANDLERS_H
-#define HANDLERS_H
-
-#include "ecewo.h"
-void hello_world(Req *req, Res *res);
-#endif
